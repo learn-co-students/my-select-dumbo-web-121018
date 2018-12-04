b749ed19d50a002fc551ec2bc32053761964cfbd
jesusg1012 <jesus.jg.medina@gmailcom>
2018-12-04 17:32:55 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,9 @@ def my_select(collection)
   arr = []
   x = 0
   while x < collection.length
-    arr.push(yield(collection.length))
+    if yield(collection[x])
+      arr.push(collection[x])
+    end
     x+=1
   end
   return arr
